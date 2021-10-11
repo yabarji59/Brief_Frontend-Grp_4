e395689758eff5699001c62d4de9ee9bc267a3f3
BERRAI <kaabiaroua@gmail.com>
2021-10-11 16:37:42 +0200

    userData json

--- TpAngular/src/app/app.module.ts
+++ TpAngular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
