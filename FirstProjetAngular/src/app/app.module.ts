1e12292a7ff44b88b2bc8bb2fe54e84fe5c543b3
karima <saidaniikarima@gmail.com>
2021-10-11 15:57:30 +0200

    feat_karima : 2emecommit

--- FirstProjetAngular/src/app/app.module.ts
+++ FirstProjetAngular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './composants/header/header.component';
+import { UserComponent } from './composants/user/user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+   
   ],
   imports: [
     BrowserModule,
